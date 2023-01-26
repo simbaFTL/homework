99b0b176ca6e5ea2fc556a2408018b89f65bd7d6
simbaFTL <120658683+simbaFTL@users.noreply.github.com>
2023-01-26 23:27:01 +0300

    Add files via upload

---
+++ Homeworks/2.4.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+	int day;
+	printf("Day? ");
+	scanf_s("%i",&day);
+
+	switch (day)
+	{
+	case 1:
+	{
+		printf("Monday");
+	}
+	break;
+	case 2:
+	{
+		printf("Tuesday");
+	}
+	break;
+	case 3:
+	{
+		printf("Wednesday");
+	}
+	break;
+	case 4:
+	{
+		printf("Thursday");
+	}
+	break;
+	case 5:
+	{
+		printf("Friday");
+	}
+	break;
+	case 6:
+	{
+		printf("Saturday");
+	}
+	break;
+	case 7:
+	{
+		printf("Sunday");
+	}
+	break;
+	default:
+	{
+		printf("Error");
+	}
+	}
+}
