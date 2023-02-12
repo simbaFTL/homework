b523849c9b13c9c6cb9de1164847259351bdcf29
simbaFTL <120658683+simbaFTL@users.noreply.github.com>
2023-02-12 23:00:45 +0300

    Add files via upload

---
+++ Homeworks/3.7.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int i = 1;
+	int sum = 0;
+	for (;i < 100;i = i + 1)
+	{
+		sum = sum + i;
+		printf("%i+", i);
+
+	}
+	sum = sum + i;
+	printf("%i=%i", i, sum);
+
+}
