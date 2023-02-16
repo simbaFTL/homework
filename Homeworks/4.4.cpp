c1935ea8983176502e419faddc4d6b11691bf39e
simbaFTL <120658683+simbaFTL@users.noreply.github.com>
2023-02-16 19:53:42 +0300

    Add files via upload

---
+++ Homeworks/4.4.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+	int mas[100];
+	int n;
+
+	printf("Elements number ");
+	scanf_s("%i",&n);
+
+	for (int i = 0;i < n;i++)
+	{
+		scanf_s("%i", &mas[i]);
+	}
+
+	int min, max;
+	min = max = mas[0];
+
+	int sum = 0;
+
+	for (int i = 0;i < n;i++)
+	{
+		if (mas[i] > max)
+		{
+			max = mas[i];
+		}
+
+		if (mas[i] < min)
+		{
+			min = mas[i];
+		}
+
+		sum += mas[i];
+	}
+
+	float sr = (float)sum / (float)n;
+
+	printf("Sum=%i Sr=%f Min=%i Max=%i", sum, sr, min, max);
+
+}
