83af5715e9698d45e37dfeb58fb75318c1ea0ea0
simbaFTL <120658683+simbaFTL@users.noreply.github.com>
2023-06-23 16:42:52 +0300

    Add files via upload

---
+++ Home work Python/2.py
@@ -0,0 +1,12 @@
+a,b=float(input()), float(input())
+print('Периметр прямоугольника =', 2*(a+b))
+print('Площадь прямоугольника =', a*b)
+
+
+num=float(input())
+a=num//1%10
+b=num//10%10
+c=num//100%10
+d=num//1000%10
+e=num//10000%10
+print(b**a*c/(e-d))
